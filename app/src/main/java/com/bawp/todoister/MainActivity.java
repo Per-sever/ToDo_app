1d85840c700a55c46e6b28b8eb121bd13ef11968
per-sever <a.petrin107@gmail.com>
2022-02-07 18:30:58 +0300

    Initial commit

---
+++ app/src/main/java/com/bawp/todoister/MainActivity.java
@@ -0,0 +1,113 @@
+package com.bawp.todoister;
+
+import android.os.Bundle;
+
+import com.bawp.todoister.adapter.OnTodoClickListener;
+import com.bawp.todoister.adapter.RecycleViewAdapter;
+import com.bawp.todoister.model.Priority;
+import com.bawp.todoister.model.SharedViewModel;
+import com.bawp.todoister.model.Task;
+import com.bawp.todoister.model.TaskViewModel;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.Calendar;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements OnTodoClickListener {
+    private  static final String TAG = "ITEM";
+    private TaskViewModel taskViewModel;
+    private RecyclerView recyclerView;
+    private RecycleViewAdapter recycleViewAdapter;
+    private int counter;
+    BottomSheetFragment bottomSheetFragment;
+    private SharedViewModel sharedViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        counter = 0;
+
+        bottomSheetFragment = new BottomSheetFragment();
+        ConstraintLayout constraintLayout = findViewById(R.id.bottomSheet);
+        BottomSheetBehavior<ConstraintLayout> bottomSheetBehavior = BottomSheetBehavior.from(constraintLayout);
+        bottomSheetBehavior.setPeekHeight(BottomSheetBehavior.STATE_HIDDEN);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        taskViewModel = new ViewModelProvider.AndroidViewModelFactory(
+                MainActivity.this.getApplication())
+                .create(TaskViewModel.class);
+
+
+        sharedViewModel = new ViewModelProvider(this).get(SharedViewModel.class);
+
+
+        taskViewModel.getAllTasks().observe(this, tasks -> {
+         recycleViewAdapter = new RecycleViewAdapter(tasks, this);
+         recyclerView.setAdapter(recycleViewAdapter);
+        });
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> {
+
+            showBottomSheetDialog();
+        });
+    }
+
+    private void showBottomSheetDialog() {
+        bottomSheetFragment.show(getSupportFragmentManager(), bottomSheetFragment.getTag());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onTodoClick(Task task) {
+        sharedViewModel.selectItem(task);
+        sharedViewModel.setIsEdit(true);
+        showBottomSheetDialog();
+    }
+
+    @Override
+    public void onToRadioButtonClick(Task task) {
+        Log.d("Click", "onRadioButton " + task.getTask());
+        TaskViewModel.delete(task);
+        recycleViewAdapter.notifyDataSetChanged();
+    }
+}
